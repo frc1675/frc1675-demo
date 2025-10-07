cbf11167cb53fe49da2e496743b08a1eed1c73ac
June <jhuablai.thao@gmail.com>
2025-10-06 20:01:08 -0500

    test your code

---
+++ src/main/java/frc/robot/Counter.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.epilogue.CustomLoggerFor;
+import edu.wpi.first.epilogue.Logged;
+
+/** Add your docs here. */
+@Logged
+public class Counter {
+    public String mySecondVariabler = "anything in here";
+    public int count = 0;
+
+    public void increment(int incrementAmount) {
+        if (count < 6) {
+        count = count + incrementAmount;
+        }
+    }
+}
