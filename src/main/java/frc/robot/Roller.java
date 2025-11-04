71c046110ec7c6688ec35fb9030eead814a2559a
UPS Laptop 1 <ups1675@gmail.com>
2025-11-03 19:51:51 -0600

    Learned how to get encoder and home switch values and how to view them

--- src/main/java/frc/robot/Roller.java
+++ src/main/java/frc/robot/Roller.java
@@ -7,12 +7,22 @@ package frc.robot;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+
 /** Add your docs here. */
+@Logged
 public class Roller {
     private final int SHOOTER_MOTOR_BOTTOM = 20;
     private final int SHOOTER_MOTOR_TOP = 17;
-    private final double MOTOR_ON_SPEED = 4.0;
+    private final double MOTOR_ON_SPEED = -6.0;
     private final double MOTOR_OFF_SPEED = 0.0;
+    private final int ENCODER_CHANNEL = 7;
+    private final int HOMESWITCH_CHANNEL = 9;
+
+    private DutyCycleEncoder encoder = new DutyCycleEncoder(ENCODER_CHANNEL);
+    private DigitalInput homeSwitch = new DigitalInput(HOMESWITCH_CHANNEL);
 
     private final SparkMax motor = new SparkMax(SHOOTER_MOTOR_BOTTOM, MotorType.kBrushless);
     private final SparkMax motor2 = new SparkMax(SHOOTER_MOTOR_TOP, MotorType.kBrushless);
@@ -38,5 +48,15 @@ public class Roller {
     public void stopMotor() {
         motor.setVoltage(0);
         motor2.setVoltage(0);
+    }
+
+    @Logged
+    public double getEncoderValue() {
+        return encoder.get();
+    }
+
+    @Logged
+    public boolean getHomeSwitchValue() {
+        return homeSwitch.get();
     }
 }
